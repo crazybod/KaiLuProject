d108ecb78fdc080240b0a5c1a8aba43928fe856f
CrazyBod <15988404491@163.com>
2019-05-08 16:51:00 +0800

    My first commit

---
+++ KL_Project/BillPlugins/CustomQuery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Kingdee.BOS;
+using Kingdee.BOS.Core.Bill.PlugIn;
+using Kingdee.BOS.Core.DynamicForm.PlugIn.Args;
+using Kingdee.BOS.Orm.DataEntity;
+using Kingdee.BOS.Core.SqlBuilder;
+using Kingdee.BOS.Core.Metadata;
+using Kingdee.BOS.App.Data;
+using Kingdee.BOS.ServiceHelper;
+using System.ComponentModel;
+using System.Data;
+using Kingdee.BOS.Core.Bill.PlugIn.Args;
+using Kingdee.BOS.Core.DynamicForm;
+using Kingdee.BOS.Contracts;
+using System.Threading;
+using Kingdee.BOS.Core.DynamicForm.PlugIn;
+using Kingdee.BOS.Core.List;
+using KL_Project.Untity;
+
+namespace KL_Project.BillPlugins
+{
+    [Description("物料自定义模糊查询")]
+    public class CustomQuery : AbstractBillPlugIn
+    {
+        //string Path = @"D:\凯路调试.txt";
+        public override void BeforeF7Select(BeforeF7SelectEventArgs e)
+        {
+            base.BeforeF7Select(e);
+            string ActionKey = e.FieldKey.ToUpper();
+            if (ActionKey == "FMATERIALID")
+            {
+                var a = this.View.GetFieldEditor("FMATERIALID", e.Row);
+                var DataSource = this.Model.DataObject;
+                //采用SQL注入来实现自定义
+                e.ListFilterParameter.SelectEntitys.Add("t0_L.FSPECIFICATION");
+                e.ListFilterParameter.SelectFields.Add("t0_L.FSPECIFICATION");
+                e.ListFilterParameter.Filter = "FMODIFIERID = 100007 AND t0.FMATERIALID IN (100612,100614,100589))))))--";
+            } 
+        }
+
+        public override void EntityRowDoubleClick(EntityRowClickEventArgs e)
+        {
+            base.EntityRowDoubleClick(e);
+            ListShowParameter Parameter = new ListShowParameter();
+            Parameter.FormId = "STK_Inventory";
+            Parameter.MultiSelect = false;
+            Parameter.ListFilterParameter.Filter = "FMATERIALID = 100612";
+            this.View.ShowForm(Parameter);
+        }
+    }
+}
---
+++ KL_Project/Untity/Log.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+
+namespace KL_Project.Untity
+{
+    [Description("保存日志")]
+    public class Log
+    {
+        /// <summary>
+        /// 写入日志文件
+        /// </summary>
+        /// <param name="LogInfo">日志信息</param>
+        /// <param name="Path">保存路径</param>
+        public static void WriteLog(string LogInfo, string Path)
+        {
+            using (StreamWriter sw = new StreamWriter(Path, true, Encoding.UTF8))
+            {
+                sw.WriteLine(LogInfo);
+            }
+        }
+
+        /// <summary>
+        /// 根据异常类型生成 消息日志
+        /// </summary>
+        /// <param name="e">异常对象</param>
+        /// <returns></returns>
+        public static string PrintStackTrace(Exception e)
+        {
+            var sb = new StringBuilder(1024);
+            sb.Append($"Exception Type {e.GetType().Name}, Message:{e.Message},Stack:{e.StackTrace}\n");
+            if (e.InnerException != null)
+            {
+                sb.Append(" inner Exception: ");
+                sb.Append(PrintStackTrace(e.InnerException));
+            }
+            return sb.ToString();
+        }
+    }
+}
